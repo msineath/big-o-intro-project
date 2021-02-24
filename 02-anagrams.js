a539399ef2f67dd757961e2dbd7833f9f49572cc
msineath <msineath@LAPTOP-65OGSQM1.localdomain>
2021-02-24 17:41:17 -0500

    finishing up secondAnagram

--- 02-anagrams.js
+++ 02-anagrams.js
@@ -4,8 +4,12 @@ function firstAnagram(str1, str2) {
   for (let i = 0; i < str1.length; i++) {
     let letter = str1[i];
     let ssi = str2.indexOf(letter);
-    if (ssi)
-      str2.split('').splice(ssi,1).join('');
+    if (ssi !== -1) {
+
+      str2 = str2.split('')
+      str2.splice(ssi,1);
+      str2 = str2.join('')
+    }
     else
       return false;
   }
@@ -15,14 +19,27 @@ function firstAnagram(str1, str2) {
     return false;
 }
 
-console.log(firstAnagram("gizmo", "sally"));    // => false
-console.log(firstAnagram("elvis", "lives"));    // => true
+// console.log(firstAnagram("gizmo", "sally"));    // => false
+// console.log(firstAnagram("elvis", "lives"));    // => true
 
 
 function secondAnagram(str1, str2) {
   // Code goes here ....
+  if(str1.length !== str2.length) {
+    return false;
+  }
+  str1.sort((a, b) => a - b)
+  str2.sort((a, b) => a - b)
+
+  for(let i = 0; i < str1.length; i++) {
+    if(str1[i] !== str2[i]) 
+    return false
+  }
+  return true
 }
 
+console.log(secondAnagram("gizmo", "sally"));    // => false
+console.log(secondAnagram("elvis", "lives"))
 
 function thirdAnagram(str1, str2) {
   // Code goes here ....
